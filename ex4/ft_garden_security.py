7c210b5f5e499ae3bba338f96073d163e8dfbfaf
hasnoussi <hasnae.snoussi2001@gmail.com>
2026-09-04 18:20:23 +0200

    Add type hints to Plant class methods

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
+
+
 class Plant:
-    def __init__(self, name, height, age_d):
-        self.name = name 
+    def __init__(self, name: str, height: float, age_d: int) -> None:
+        self.name = name
         self._height = height
         self._age_d = age_d
-    
-    def grow(self):
+
+    def grow(self) -> None:
         self._height = round(self._height + 0.8, 1)
-    def age(self):
+
+    def age(self) -> None:
         self._age_d = self._age_d + 1
-    def show(self):
+
+    def show(self) -> None:
         print(f"{self.name}: {self._height}cm, {self._age_d} days old")
-    def  track_week(self):
+
+    def track_week(self) -> None:
         self.show()
         height_debut = self._height
         for i in range(7):
@@ -21,15 +26,16 @@ class Plant:
             self.show()
         croissance = round(self._height - height_debut, 1)
         print(f"Growth this week: {croissance}cm")
-    
-    def set_height(self, nouvelle_valeur):
+
+    def set_height(self, nouvelle_valeur: float) -> None:
         if nouvelle_valeur >= 0:
             self._height = nouvelle_valeur
             print(f"Height updated: {self._height:.0f}cm")
         else:
             print(f"{self.name}: Error, height can't be negative")
             print("Height update rejected")
-    def set_age(self, nouvelle_valeur): 
+
+    def set_age(self, nouvelle_valeur: int) -> None:
         if nouvelle_valeur >= 0:
             self._age_d = nouvelle_valeur
             print(f"Age updated: {nouvelle_valeur} days")
@@ -38,17 +44,18 @@ class Plant:
             print(f"{self.name}: Error, age can't be negative")
             print("Age update rejected")
             print()
-    def get_height(self):
+
+    def get_height(self) -> float:
         return self._height
-    def get_age(self):
+
+    def get_age(self) -> int:
         return self._age_d
 
-              
+
 if __name__ == "__main__":
-    
     print("=== Garden Security System ===")
     p1 = Plant("Rose", 15.0, 10)
-    print(f"Plant created: ", end="")
+    print("Plant created: ", end="")
     p1.show()
     print()
 
@@ -61,4 +68,3 @@ if __name__ == "__main__":
 
     print("Current state: ", end="")
     p1.show()
-    
